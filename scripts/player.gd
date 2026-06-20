1080055795dae909b4f979cf4fb5fb751a58a7d9
MlSHl <mishi.beraia@gmail.com>
2026-06-21 03:24:17 +0400

    Added vertical drop camera movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,34 +1,47 @@
 extends CharacterBody3D
 
 
-const SPEED = 25.0
+const SPEED = 10.0
 const ROTATION_SPEED = 2.5
-const ROLLING_SPEED = 25.0
-const MAX_CAMERA_DELTA := 0.6
+const ROLLING_SPEED = 10.0
+const MAX_CAMERA_DELTA_X := 0.2
+const MAX_CAMERA_DELTA_Y := 0.6
 
-var camera_delta := 0.0
+var camera_delta_x := 0.0
+var camera_delta_y := 0.0
 
 @onready var camera: Camera3D = $Camera3D
+@onready var camera_x := camera.position.x
+@onready var camera_y := camera.position.y
+@export var gravity_multiplier := 5.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += gravity_multiplier * get_gravity() * delta # F/m
+		if camera_delta_y < MAX_CAMERA_DELTA_Y: 
+			camera_delta_y += delta
+			camera.position.y = camera_y + camera_delta_y
+	else: 
+		if abs(camera_delta_y) > 0:
+			camera_delta_y -= sign(camera_delta_y) * delta
+			camera.position.y = camera_y + camera_delta_y
+			if abs(camera_delta_y) <= 0.01:
+				camera_delta_y = 0
 		
 	# Turning left/right
 	var input_dir := Input.get_axis("left", "right")
 	rotation.y += -input_dir * ROTATION_SPEED * delta
 	if input_dir:
-		if abs(camera_delta) < MAX_CAMERA_DELTA:
-			camera_delta += input_dir * delta
-			camera.position.x += delta * input_dir
+		if abs(camera_delta_x) < MAX_CAMERA_DELTA_X:
+			camera_delta_x += 	input_dir * delta
+			camera.position.x = camera_x + camera_delta_x 
 	else:
-		if abs(camera_delta) > 0:
-			camera_delta += -sign(camera_delta) * delta
-			camera.position.x -= delta * sign(camera_delta)
-			if abs(camera_delta) <= 0.01:
-				camera_delta = 0
-		
+		if abs(camera_delta_x) > 0:
+			camera_delta_x -= sign(camera_delta_x) * delta
+			camera.position.x = camera_x + camera_delta_x
+			if abs(camera_delta_x) <= 0.01:
+				camera_delta_x = 0
 		
 	# Constant foward velocity
 	var forward := -transform.basis.z
