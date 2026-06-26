0241f6320ae4d795c44f062dc6ea40ef613b5ae1
crows-on-a-roof <263181222+crows-on-a-roof@users.noreply.github.com>
2026-06-26 10:33:23 +0200

    audiocontroller

--- scripts/marathon.gd
+++ scripts/marathon.gd
@@ -4,6 +4,17 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SceneTransition._fade_from_white()
+	
+	
+	
+	AudioController.running_song_1.play()
+	AudioController.running_song_1.stop()
+	
+	AudioController.glass_shatter.play()
+	
+
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
